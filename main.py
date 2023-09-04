fix: switching to higher precision for jax

main.py
@@ -43,9 +43,7 @@ class App:
         self._model_y_b = np.zeros(self._instances, dtype=np.float32)
         self._framebuffer_size_changed = False
 
-        self._jax_float = (
-            jnp.float64 if jax.config.jax_default_dtype_bits == "64" else jnp.float32
-        )
+        self._jax_float = jnp.float64 if jax.config.jax_enable_x64 else jnp.float32
 
         self._rectanguloids = []
         self._init_rectanguloids()

test_collisions.py
@@ -6,7 +6,7 @@ import math
 
 class TestSphereRectanguloidCollisions:
     def test_no_collision(self):
-        sphere = collisions.Sphere(jnp.array([0, 0, 0], dtype=jnp.float32), 1.0)
+        sphere = collisions.Sphere(jnp.array([0, 0, 0], dtype=jnp.float32), 1.0, 1.0)
         rectanguloid = collisions.Rectanguloid(
             jnp.array([2, 2, 2], dtype=jnp.float32),
             jnp.array([3, 3, 3], dtype=jnp.float32),
@@ -17,7 +17,7 @@ class TestSphereRectanguloidCollisions:
         assert jnp.isnan(intersections).all()
 
     def test_no_collision(self):
-        sphere = collisions.Sphere(jnp.array([0, 1.0, 0], dtype=jnp.float32), 0.49)
+        sphere = collisions.Sphere(jnp.array([0, 1.0, 0], dtype=jnp.float32), 0.49, 1.0)
         rectanguloid = collisions.Rectanguloid(
             jnp.array([-0.5, -0.5, -0.5], dtype=jnp.float32),
             jnp.array([0.5, 0.5, 0.5], dtype=jnp.float32),
@@ -28,7 +28,7 @@ class TestSphereRectanguloidCollisions:
         assert jnp.isnan(intersections).all()
 
     def test_collision_just_touching(self):
-        sphere = collisions.Sphere(jnp.array([0, 1.0, 0], dtype=jnp.float32), 0.5)
+        sphere = collisions.Sphere(jnp.array([0, 1.0, 0], dtype=jnp.float32), 0.5, 1.0)
         rectanguloid = collisions.Rectanguloid(
             jnp.array([-0.5, -0.5, -0.5], dtype=jnp.float32),
             jnp.array([0.5, 0.5, 0.5], dtype=jnp.float32),
@@ -54,7 +54,7 @@ class TestSphereRectanguloidCollisions:
         ).all()
 
     def test_collision_a(self):
-        sphere = collisions.Sphere(jnp.array([0, 1.0, 0], dtype=jnp.float32), 0.51)
+        sphere = collisions.Sphere(jnp.array([0, 1.0, 0], dtype=jnp.float32), 0.51, 1.0)
         rectanguloid = collisions.Rectanguloid(
             jnp.array([-0.5, -0.5, -0.5], dtype=jnp.float32),
             jnp.array([0.5, 0.5, 0.5], dtype=jnp.float32),
@@ -84,6 +84,7 @@ class TestSphereRectanguloidCollisions:
             jnp.array([0, 1.0, 0], dtype=jnp.float32),
             # Here I am essentially adding a small epsilon to the radius to guarantee intersection with the side faces.
             jnp.float32(math.sqrt(1 / 2.0) + 0.01),
+            1.0,
         )
         rectanguloid = collisions.Rectanguloid(
             jnp.array([-0.5, -0.5, -0.5], dtype=jnp.float32),
@@ -114,6 +115,7 @@ class TestSphereRectanguloidCollisions:
         sphere = collisions.Sphere(
             jnp.array([0, -1.0, 0], dtype=jnp.float32),
             jnp.float32(math.sqrt(1 / 2.0) - 0.01),
+            1.0,
         )
         rectanguloid = collisions.Rectanguloid(
             jnp.array([-0.5, -0.5, -0.5], dtype=jnp.float32),
@@ -142,8 +144,7 @@ class TestSphereRectanguloidCollisions:
 
     def test_corner_collision_rectanguloid_off_center(self):
         sphere = collisions.Sphere(
-            jnp.array([10.55, 15.3, 21.05], dtype=jnp.float32),
-            jnp.float32(0.1),
+            jnp.array([10.55, 15.3, 21.05], dtype=jnp.float32), jnp.float32(0.1), 1.0
         )
         rectanguloid = collisions.Rectanguloid(
             jnp.array([10.0, 15.0, 20.0], dtype=jnp.float32),
@@ -172,8 +173,7 @@ class TestSphereRectanguloidCollisions:
 
     def test_collision_sphere_inside_rectanguloid(self):
         sphere = collisions.Sphere(
-            jnp.array([0, 0.5, 0], dtype=jnp.float32),
-            jnp.float32(0.1),
+            jnp.array([0, 0.5, 0], dtype=jnp.float32), jnp.float32(0.1), 1.0
         )
         rectanguloid = collisions.Rectanguloid(
             jnp.array([-0.5, -0.5, -0.5], dtype=jnp.float32),
@@ -190,6 +190,35 @@ class TestSphereRectanguloidCollisions:
                 sphere.center,
                 sphere.center,
                 sphere.center,
+            ],
+            dtype=jnp.float32,
+        )
+        nan_element_mask = jnp.isnan(intersections)
+        assert (nan_element_mask == jnp.isnan(expected_intersections)).all()
+        nan_mask = jnp.all(nan_element_mask, axis=1)
+        assert jnp.equal(
+            intersections[~nan_mask], expected_intersections[~nan_mask]
+        ).all()
+
+    def test_collision_sphere_flat_rectanguloid(self):
+        sphere = collisions.Sphere(
+            jnp.array([10.55, 15.1, 21.05], dtype=jnp.float32), jnp.float32(0.15), 1.0
+        )
+        rectanguloid = collisions.Rectanguloid(
+            jnp.array([10.0, 15.0, 20.0], dtype=jnp.float32),
+            jnp.array([10.5, 15.0, 21.0], dtype=jnp.float32),
+        )
+        intersections = jax.jit(collisions.collide_sphere_rectanguloid)(
+            sphere, rectanguloid
+        )
+        expected_intersections = jnp.array(
+            [
+                [jnp.nan, jnp.nan, jnp.nan],
+                [10.5, 15.25, 21.0],
+                [jnp.nan, jnp.nan, jnp.nan],
+                [10.5, 15.25, 21.0],
+                [jnp.nan, jnp.nan, jnp.nan],
+                [10.5, 15.25, 21.0],
             ],
             dtype=jnp.float32,
         )