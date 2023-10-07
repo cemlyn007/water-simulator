fix: adjust water normal calculation to work for jax-metal 0.0.4
There appears to be an issue using the `keepdims=True` in the `jnp.linalg.norm` when running on METAL with jax-metal 0.0.4.

meshes/water.py
@@ -343,5 +343,5 @@ class WaterVertexNormalUpdater:
             faces_vertices[:, 1, :] - faces_vertices[:, 0, :],
             faces_vertices[:, 2, :] - faces_vertices[:, 0, :],
         )
-        normals /= jnp.linalg.norm(normals, axis=1, keepdims=True)
+        normals /= jnp.linalg.norm(normals, axis=1)[:, None]
         return normals