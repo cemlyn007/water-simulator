175c3468122f33b1154b663a5b198f5055655885
Cemlyn Waters <cemlynwaters@gmail.com>
2024-06-09 11:16:23 +0100

    refactor: format files

--- water_simulator/shaders/basic_lighting.fs
+++ water_simulator/shaders/basic_lighting.fs
@@ -24,7 +24,7 @@ void main() {
     float ambientStrength = 0.1;
     vec3 ambient = ambientStrength * lightColor;
 
-    // diffuse 
+    // diffuse
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
